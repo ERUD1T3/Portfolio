73decd6b9c6f4bdffac7cc7da3b146d93279f069
jmoukpe <32252266+jmoukpe@users.noreply.github.com>
2018-11-24 14:07:07 -0500

    update

---
+++ FrontEnd/signup_page/sign_up.js
@@ -0,0 +1,6 @@
+
+function main() {
+    var username = document.getElementById("username");
+    var password = document.getElementById("password");
+    console.log("user = " + username + ", password = " + password);
+}
