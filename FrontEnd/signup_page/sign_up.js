7d94af1ad66ce0778fffaf52f43badddabc71110
jmoukpe <32252266+jmoukpe@users.noreply.github.com>
2018-11-27 12:47:16 -0500

    fixed the inital issue with the sign using functional programming

--- FrontEnd/signup_page/sign_up.js
+++ FrontEnd/signup_page/sign_up.js
@@ -1,7 +1,17 @@
-var usr = document.getElementById("username"); //getting username 
-var pwd = document.getElementById("password"); //getting password
+//Multifarious Final Project
+
+//var usr = document.getElementById("username").value; //getting username 
+//var pwd = document.getElementById("password").value; //getting password
 
 function main() {
-    alert(usr);
-    alert("user =  , password = ${pwd}"); //printing username and password
+    //event.target.matches("id")
+    var usr = document.getElementById("username").value; //getting username 
+    var pwd = document.getElementById("password").value; //getting password
+    console.log("user = " + usr + ",  password = "  + pwd); //printing username and password
 }
+
+/*
+window.addEventListener("load", function(event) {
+    Document.addEventListener("click", main, false);
+  });
+*/
