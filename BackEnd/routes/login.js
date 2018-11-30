9f59bc4346f72f244ef8347c4a44d3e14faac273
Josias Moukpe <josiasmoukpe@gmail.com>
2018-11-30 15:28:11 -0500

    moved login.js into routes/

--- BackEnd/routes/login.js
+++ BackEnd/routes/login.js
@@ -1,7 +1,5 @@
 //file to encapsulate the routes to the server
 const express = require('express') //importing express packages
-const app = express() //creating a express instance 
-const morgan = require('morgan')
 const mysql = require('mysql')
 const bodyParser = require('body-parser')
 
